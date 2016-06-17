db0175d5a23f9ef76626295e2f18d3194da90050
Dan Bikle <bikle@bikle.com>
2016-06-17 15:56:39 -0700

    more

---
+++ public/class03/fit3.py
@@ -0,0 +1,13 @@
+# fit3.py
+
+# This script should use NumPy and scikit-learn to fit a line to three points.
+
+import numpy as np
+import pdb
+
+pdb.set_trace()
+pt0 = [0.001,0.001]
+pt1 = [1.1,1.1]
+pt2 = [2.2,2.2]
+
+'bye'
