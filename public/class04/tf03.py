aeaf9bee88ab80cc7bd6119101b96746c8c5b31c
Dan Bikle <bikle@bikle.com>
2016-06-24 13:46:50 -0700

    more

--- public/class04/tf03.py
+++ public/class04/tf03.py
@@ -28,41 +28,6 @@ tf.app.flags.DEFINE_integer('num_hidden', 1,
                             'Number of nodes in the hidden layer.')
 tf.app.flags.DEFINE_boolean('verbose', False, 'Produce verbose output.')
 FLAGS   = tf.app.flags.FLAGS
-verbose = FLAGS.verbose
-
-# I should use Pandas and Numpy here:
-data_df  = pd.read_csv('http://www.spy611.com/csv/allpredictions.csv')
-train_sr = (data_df['cdate'] > '2010') & (data_df['cdate'] < '2016')
-test_sr  =  data_df['cdate'] > '2016'
-train_df = data_df[['actual_dir', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][train_sr]
-test_df  = data_df[['actual_dir', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][test_sr]
-train_a  = np.array(train_df)
-test_a   = np.array(test_df)
-train_data       = train_a[:,1:].astype(np.float32)
-test_data        = test_a[:,1:].astype(np.float32)
-train_labels_i_a = train_a[:,0].astype(dtype=np.uint8)
-test_labels_i_a  = test_a[:,0].astype(dtype=np.uint8)
-# Convert the int numpy array into a one-hot matrix.
-train_labels     = (np.arange(NUM_LABELS) == train_labels_i_a[:, None]).astype(np.float32)
-test_labels      = (np.arange(NUM_LABELS) == test_labels_i_a[:, None]).astype(np.float32)
-    
-# Get the shape of the training data.
-train_size,num_features = train_data.shape
-
-# Get the number of epochs for training.
-num_epochs = FLAGS.num_epochs
-
-# Get the size of layer one.
-num_hidden = FLAGS.num_hidden
-
-# This is where training samples and labels are fed to the graph.
-# These placeholder nodes will be fed a batch of training data at each
-# training step using the {feed_dict} argument to the Run() call below.
-x  = tf.placeholder("float", shape=[None, num_features])
-y_ = tf.placeholder("float", shape=[None, NUM_LABELS])
-
-# For the test data, hold the entire dataset in one constant node.
-test_data_node = tf.constant(test_data)
 
 # Init weights method. (Lifted from Delip Rao: http://deliprao.com/archives/100)
 def init_weights(shape, init_method='xavier', xavier_params = (None, None)):
@@ -76,62 +41,100 @@ def init_weights(shape, init_method='xavier', xavier_params = (None, None)):
         high = 4*np.sqrt(6.0/(fan_in + fan_out))
         return tf.Variable(tf.random_uniform(shape, minval=low, maxval=high, dtype=tf.float32))
 
-# Define and initialize the network.
-
-# Initialize the hidden weights and biases.
-w_hidden = init_weights(
-    [num_features, num_hidden],
-    'xavier',
-    xavier_params=(num_features, num_hidden))
-
-
-b_hidden = init_weights([1,num_hidden],'zeros')
-
-# The hidden layer.
-hidden = tf.nn.tanh(tf.matmul(x,w_hidden) + b_hidden)
-
-# Initialize the output weights and biases.
-w_out = init_weights(
-    [num_hidden, NUM_LABELS],
-    'xavier',
-    xavier_params=(num_hidden, NUM_LABELS))
-
-b_out = init_weights([1,NUM_LABELS],'zeros')
-
-# The output layer.
-y = tf.nn.softmax(tf.matmul(hidden, w_out) + b_out)
-
-# Optimization.
-cross_entropy = -tf.reduce_sum(y_*tf.log(y))
-train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-
-# Evaluation.
-correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-
-# Create a local session to run this computation.
-with tf.Session() as s:
-    # Run all the initializers to prepare the trainable parameters.
-	tf.initialize_all_variables().run()
-	if verbose:
-	    print('Initialized!')
-	    print()
-	    print('Training.')
-	    
-	# Iterate and train.
-	for step in range(num_epochs * train_size // BATCH_SIZE):
-	    if verbose:
-	        print(step,)
-	        
-	    offset = (step * BATCH_SIZE) % train_size
-	    batch_data = train_data[offset:(offset + BATCH_SIZE), :]
-	    batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
-	    train_step.run(feed_dict={x: batch_data, y_: batch_labels})
-	    if verbose and offset >= train_size-BATCH_SIZE:
-	        print()
-	print("Accuracy:", accuracy.eval(feed_dict={x: test_data, y_: test_labels}))
-
-tf.app.run()
+def main(argv=None):
+  verbose = FLAGS.verbose
+  
+  # I should use Pandas and Numpy here:
+  data_df  = pd.read_csv('http://www.spy611.com/csv/allpredictions.csv')
+  train_sr = (data_df['cdate'] > '2010') & (data_df['cdate'] < '2016')
+  test_sr  =  data_df['cdate'] > '2016'
+  train_df = data_df[['actual_dir', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][train_sr]
+  test_df  = data_df[['actual_dir', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][test_sr]
+  train_a  = np.array(train_df)
+  test_a   = np.array(test_df)
+  train_data       = train_a[:,1:].astype(np.float32)
+  test_data        = test_a[:,1:].astype(np.float32)
+  train_labels_i_a = train_a[:,0].astype(dtype=np.uint8)
+  test_labels_i_a  = test_a[:,0].astype(dtype=np.uint8)
+  # Convert the int numpy array into a one-hot matrix.
+  train_labels     = (np.arange(NUM_LABELS) == train_labels_i_a[:, None]).astype(np.float32)
+  test_labels      = (np.arange(NUM_LABELS) == test_labels_i_a[:, None]).astype(np.float32)
+      
+  # Get the shape of the training data.
+  train_size,num_features = train_data.shape
+  
+  # Get the number of epochs for training.
+  num_epochs = FLAGS.num_epochs
+  
+  # Get the size of layer one.
+  num_hidden = FLAGS.num_hidden
+  
+  # This is where training samples and labels are fed to the graph.
+  # These placeholder nodes will be fed a batch of training data at each
+  # training step using the {feed_dict} argument to the Run() call below.
+  x  = tf.placeholder("float", shape=[None, num_features])
+  y_ = tf.placeholder("float", shape=[None, NUM_LABELS])
+  
+  # For the test data, hold the entire dataset in one constant node.
+  test_data_node = tf.constant(test_data)
+  
+  # Define and initialize the network.
+  
+  # Initialize the hidden weights and biases.
+  w_hidden = init_weights(
+      [num_features, num_hidden],
+      'xavier',
+      xavier_params=(num_features, num_hidden))
+  
+  
+  b_hidden = init_weights([1,num_hidden],'zeros')
+  
+  # The hidden layer.
+  hidden = tf.nn.tanh(tf.matmul(x,w_hidden) + b_hidden)
+  
+  # Initialize the output weights and biases.
+  w_out = init_weights(
+      [num_hidden, NUM_LABELS],
+      'xavier',
+      xavier_params=(num_hidden, NUM_LABELS))
+  
+  b_out = init_weights([1,NUM_LABELS],'zeros')
+  
+  # The output layer.
+  y = tf.nn.softmax(tf.matmul(hidden, w_out) + b_out)
+  
+  # Optimization.
+  cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+  train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+  
+  # Evaluation.
+  correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+  
+  # Create a local session to run this computation.
+  with tf.Session() as s:
+      # Run all the initializers to prepare the trainable parameters.
+        tf.initialize_all_variables().run()
+        if verbose:
+            print('Initialized!')
+            print()
+            print('Training.')
+            
+        # Iterate and train.
+        for step in range(num_epochs * train_size // BATCH_SIZE):
+            if verbose:
+                print(step,)
+                
+            offset = (step * BATCH_SIZE) % train_size
+            batch_data = train_data[offset:(offset + BATCH_SIZE), :]
+            batch_labels = train_labels[offset:(offset + BATCH_SIZE)]
+            train_step.run(feed_dict={x: batch_data, y_: batch_labels})
+            if verbose and offset >= train_size-BATCH_SIZE:
+                print()
+        print("Accuracy:", accuracy.eval(feed_dict={x: test_data, y_: test_labels}))
+  
+if __name__ == '__main__':
+    tf.app.run()
 
 'bye'
 
