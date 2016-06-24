f829fe509da7d388e217a197bd1ffce9d02f7a21
Dan Bikle <bikle@bikle.com>
2016-06-24 13:07:34 -0700

    more

--- public/class04/tf03.py
+++ public/class04/tf03.py
@@ -27,7 +27,8 @@ tf.app.flags.DEFINE_integer('num_epochs', 1,
 tf.app.flags.DEFINE_integer('num_hidden', 1,
                             'Number of nodes in the hidden layer.')
 tf.app.flags.DEFINE_boolean('verbose', False, 'Produce verbose output.')
-FLAGS = tf.app.flags.FLAGS
+FLAGS   = tf.app.flags.FLAGS
+verbose = FLAGS.verbose
 
 # I should use Pandas and Numpy here:
 data_df  = pd.read_csv('http://www.spy611.com/csv/allpredictions.csv')
@@ -37,6 +38,13 @@ train_df = data_df[['actual_dir', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'p
 test_df  = data_df[['actual_dir', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][test_sr]
 train_a  = np.array(train_df)
 test_a   = np.array(test_df)
+train_data       = train_a[:,1:].astype(np.float32)
+test_data        = test_a[:,1:].astype(np.float32)
+train_labels_i_a = train_a[:,0].astype(dtype=np.uint8)
+test_labels_i_a  = test_a[:,0].astype(dtype=np.uint8)
+# Convert the int numpy array into a one-hot matrix.
+train_labels     = (np.arange(NUM_LABELS) == train_labels_i_a[:, None]).astype(np.float32)
+test_labels      = (np.arange(NUM_LABELS) == test_labels_i_a[:, None]).astype(np.float32)
 
 pdb.set_trace()
 test_a[:5]
