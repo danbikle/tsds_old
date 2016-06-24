674135c269ee0f6ad0748516ebbddff61a9330d0
Dan Bikle <bikle@bikle.com>
2016-06-24 15:12:18 -0700

    more

--- public/class04/prep_gspc.py
+++ public/class04/prep_gspc.py
@@ -1,22 +1,20 @@
 # prep_gspc.py
 
-# This script should prep /tmp/gspc_train.csv and /tmp/gspc_test.csv for tf021.bash
+# This script should prep /tmp/gspc_train.csv and /tmp/gspc_test.csv for TensorFlow.
 
 # Demo:
 # python prep_gspc.py
 
-import numpy  as np
 import pandas as pd
-import pdb
 
-# I should use Pandas and Numpy here:
+# I should use Pandas here:
 data_df  = pd.read_csv('http://www.spy611.com/csv/allpredictions.csv')
 train_sr = (data_df['cdate'] > '2000') & (data_df['cdate'] < '2016')
 test_sr  =  data_df['cdate'] > '2016'
 train_df = data_df[['actual_dir', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][train_sr]
 test_df  = data_df[['actual_dir', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][test_sr]
 
-train_df.to_csv('/tmp/gspc_train.csv', float_format='%4.3f', index=False)
-test_df.to_csv( '/tmp/gspc_test.csv' , float_format='%4.3f', index=False)
+train_df.to_csv('/tmp/gspc_train.csv', float_format='%4.3f', index=False, header=False)
+test_df.to_csv( '/tmp/gspc_test.csv' , float_format='%4.3f', index=False, header=False)
 
 'bye'
