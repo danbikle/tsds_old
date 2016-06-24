758f5825d815e0b1dc4ba05e0eafecafb4eaad78
Dan Bikle <bikle@bikle.com>
2016-06-24 13:33:50 -0700

    more

--- public/class04/tf03.py
+++ public/class04/tf03.py
@@ -90,5 +90,26 @@ b_hidden = init_weights([1,num_hidden],'zeros')
 # The hidden layer.
 hidden = tf.nn.tanh(tf.matmul(x,w_hidden) + b_hidden)
 
+# Initialize the output weights and biases.
+w_out = init_weights(
+    [num_hidden, NUM_LABELS],
+    'xavier',
+    xavier_params=(num_hidden, NUM_LABELS))
+
+b_out = init_weights([1,NUM_LABELS],'zeros')
+
+# The output layer.
+y = tf.nn.softmax(tf.matmul(hidden, w_out) + b_out)
+
+# Optimization.
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+# Evaluation.
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+
+# Create a local session to run this computation.
+
 'bye'
 
