f7f3eb3920ee2cc85a694b1ada1e0991d82e8688
Dan Bikle <bikle@bikle.com>
2016-06-25 06:47:51 -0700

    more

---
+++ public/class04/sklearn_is_installed.py
@@ -0,0 +1,8 @@
+# sklearn_is_installed.py
+
+# This script should help me see if scikit-learn is installed.
+
+# If scikit-learn is installed, the line below should not fail:
+import sklearn
+
+'bye'
