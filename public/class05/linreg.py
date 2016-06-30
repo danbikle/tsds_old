92d6e9ec53dcd3303baca2dfa02699891d23d33b
Dan Bikle <bikle@bikle.com>
2016-06-30 11:57:30 -0700

    more

--- public/class05/linreg.py
+++ public/class05/linreg.py
@@ -10,11 +10,26 @@
 
 import pdb
 import pandas as pd
+import numpy  as np
 
 csv_df = pd.read_csv('http://www.spy611.com/csv/allpredictions.csv')[2599:8900]
+
+train_df = csv_df[['pctlead', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']]
+train_a  = np.array(train_df)
+
 pdb.set_trace()
-csv_df.head()
 csv_df.tail()
+train_a[-4:]
+
+x_train_a = train_a[:,1:]
+y_train_a = train_a[:,0]
+
+# I should learn from x_train_a,y_train_a:
+from sklearn import linear_model
+clf_lr = linear_model.LinearRegression()
+clf_lr.fit(x_train_a, y_train_a)
+
+# Now that I have learned, I should predict:
 
 'bye'
 # 
