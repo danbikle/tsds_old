e81269da4a107c022a5c038c316a8308fe9c03e0
Dan Bikle <bikle@bikle.com>
2016-06-29 20:11:09 -0700

    more

--- public/class05/linreg.r
+++ public/class05/linreg.r
@@ -16,6 +16,26 @@ typeof(x_l)
 y_is = csv_l[ , 3]
 head(y_is)
 
+# Now I should learn:
 mymodel = lm(y_is ~ pctlag1 + pctlag2 + pctlag4, data=x_l)
 mymodel
 
+# Now I should predict one observation:
+firstx = x_l[1,]
+aprediction = predict(mymodel,firstx)
+aprediction
+
+# Now I should predict one observation (quiet day):
+just1x = list(pctlag1=0.001,pctlag2=0.001,pctlag4=0.001)
+aprediction = predict(mymodel,just1x)
+aprediction
+
+# Now I should predict one observation (strong down day):
+just1x = list(pctlag1=-2.1,pctlag2=-3.1,pctlag4=-3.2)
+aprediction = predict(mymodel,just1x)
+aprediction
+
+# Now I should predict one observation (strong up day):
+just1x = list(pctlag1=2.1,pctlag2=2.4,pctlag4=3.2)
+aprediction = predict(mymodel,just1x)
+aprediction
