28248c165db25f38c16aefc4e223a2869909b08a
Dan Bikle <bikle@bikle.com>
2016-07-15 15:05:36 -0700

    Ishould-script-rails-s

---
+++ script/railss.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ~/tsds/script/railss.bash
+
+# This script should start rails server on all IPs so I can see the app from another host.
+
+cd ~/tsds/
+bin/rails s -b 0.0.0.0
+# 0.0.0.0 declares that I want rails listening on all IPs bound to this host.
+exit
+
