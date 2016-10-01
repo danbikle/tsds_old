3bf991598959f0f17d9101aa95c34c7e27aa6c06
Dan Bikle <bikle@bikle.com>
2016-09-30 21:59:14 -0700

    more

--- public/class04/genf.py
+++ public/class04/genf.py
@@ -54,7 +54,14 @@ for slope_i in slopes_a:
   col_s          = 'slope'+str(slope_i)
   slope_sr       = 100.0 * (rollx.mean().cp - rollx.mean().cp.shift(1))/rollx.mean().cp
   gspc_df[col_s] = slope_sr
+# I should generate Date features:
+dt_sr = pd.to_datetime(gspc_df.cdate)
+dow_l = [ float(dt.strftime('%w' ))/100.0 for dt in dt_sr ]
+moy_l = [ float(dt.strftime('%-m'))/100.0 for dt in dt_sr ]
+gspc_df['dow'] = dow_l
+gspc_df['moy'] = moy_l
 
 # I should write to CSV file to be used later:
 gspc_df.to_csv('feat.csv', float_format='%4.4f', index=False)
+
 'bye'
