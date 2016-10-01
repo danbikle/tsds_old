21259fc5d0bbb0eb9ad27a3f7bc5783c6b9cb3b6
Dan Bikle <bikle@bikle.com>
2016-09-30 17:56:12 -0700

    more

---
+++ public/class04/gbrt10.py
@@ -0,0 +1,11 @@
+# ~/tsds/public/class04/gbrt10.py
+
+# This script should demo Gradient Boosted Regression Trees (GBRT).
+# Ref:
+# http://scikit-learn.org/stable/modules/ensemble.html#gradient-boosting
+# https://en.wikipedia.org/wiki/Gradient_boosting
+# http://scikit-learn.org/stable/modules/ensemble.html#regression
+
+
+'bye'
+
