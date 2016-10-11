6bc4923db3009db82e66053583c61e8927a9754b
Dan Bikle <bikle@bikle.com>
2016-10-10 18:13:30 -0700

    more

---
+++ public/class05/moy_hist.r
@@ -0,0 +1,9 @@
+# moy_hist.r
+
+# This script should sum pctlead and groupby month of year
+# Demo:
+# R -f moy_hist.r
+
+csv_l  = read.csv('http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC')
+summary(csv_l)
+# under construction
