43b56941d961ce046bf69a4e8700916439743a2a
Dan Bikle <bikle@bikle.com>
2016-10-22 20:06:20 -0700

    more

--- public/class07/hm14.py
+++ public/class07/hm14.py
@@ -11,16 +11,21 @@ import pandas as pd
 from bokeh.charts   import HeatMap, bins, output_file, show
 from bokeh.layouts  import column, gridplot
 from bokeh.palettes import RdYlGn6, RdYlGn9
-
 import pdb
 
-yr_i     = 2010
+yr_i     = 1990
 yr_s     = str(yr_i)
 feat0_df = pd.read_csv("feat.csv")
-feat1_df = feat0_df[['cdate','pctlead','dow','moy']]
+feat1_df = feat0_df[['cdate','pctlead','pctlag1','dow','moy']]
 #cdate_sr = (feat1_df.cdate > yr_s) & (feat1_df.cdate < str(yr_i+1))
 cdate_sr = (feat1_df.cdate > yr_s)
 feat2_df = feat1_df[cdate_sr]
+
+lagdown_sr = (feat2_df.pctlag1 < 0.0)
+lagup_sr   = (feat2_df.pctlag1 > 0.0)
+lagdown_df = feat2_df[lagdown_sr]
+lagup_df   = feat2_df[lagup_sr]
+
 tools_s  = 'box_zoom,box_select,crosshair,resize,reset,save,help'
 
 hm0 = HeatMap(feat2_df
@@ -92,10 +97,24 @@ hm5 = HeatMap(feat2_df
               ,plot_height  =900
               )
 
-
+hm6 = HeatMap(lagdown_df
+              ,x     ='dow'
+              ,y     ='moy'
+              ,values='pctlead'
+              ,stat  ='mean'
+              ,tools = tools_s
+              ,legend='top_right'
+              ,title ='Lagdown Heatmap'
+              ,xlabel='Day Of Week'
+              ,ylabel='Month Of Year'
+              ,palette      =RdYlGn9
+              ,spacing_ratio=0.9
+              ,width        =900
+              ,plot_height  =900
+              )
 
 output_file('hm14.html', title='Bokeh heatmap example (hm14.py)')
 
-show(column(hm4,hm5))
+show(column(hm5,hm6))
 
 'bye'
