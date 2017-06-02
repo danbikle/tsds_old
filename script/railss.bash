33ddaa31768f084efcedb3801c1e6edd1e1b4d3d
Dan Bikle <bikle@bikle.com>
2017-06-02 16:44:58 -0700

    more

--- script/railss.bash
+++ script/railss.bash
@@ -4,7 +4,8 @@
 
 # This script should start rails server on all IPs so I can see the app from another host.
 
-cd ~/tsds/
+export BINDIR=`realpath $0|xargs dirname`
+cd    $BINDIR/.. # I should be in parent folder of bin:
 bin/rails s -b 0.0.0.0 -p 3010
 # 0.0.0.0 declares that I want rails listening on all IPs bound to this host.
 exit
