1dccfe96d68d2b49cefe7a2e516e0cf2d4c391aa
Dan Bikle <bikle@bikle.com>
2017-06-02 18:19:24 -0700

    more

--- public/class01/corr.py
+++ public/class01/corr.py
@@ -1,19 +1,20 @@
-# corr.py
+"""
+corr.py
+For 2016, this script should calculate correlation between day1-gain and day2-gain.
+"""
 
-# For 2016,
-# This script should calculate correlation between day1-gain and day2-gain.
-
+import numpy  as np
 import pandas as pd
 
 my_df    = pd.read_csv('http://www.spy611.com/csv/allpredictions.csv')
-sp0_pred = my_df['cdate'] > '2016'
-sp0_df   = my_df[sp0_pred]
-day1g_sr = sp0_df['pctlag1']
-day2g_sr = sp0_df['pctlead']
+sp0_sr   = my_df.cdate > '2016'
+sp0_df   = my_df[sp0_sr]
+day1g_sr = sp0_df.pctlag1
+day2g_sr = sp0_df.pctlead
 
 print('For 2016,')
 print('Correlation between day1-gain and day2-gain:')
-import numpy as np
+
 mycorr = np.corrcoef(day1g_sr, day2g_sr)[0,1]
 print(mycorr)
 
