c21eae3d71bf1cac98159939fb76e813dccea4e0
Dan Bikle <bikle@bikle.com>
2017-06-11 12:54:00 -0700

    more

---
+++ public/class02/mvem.bash
@@ -0,0 +1,24 @@
+
+mv Screenshot*12-17-37.png vbi12-17-37.png
+mv Screenshot*12-20-17.png vbi12-20-17.png
+mv Screenshot*12-20-39.png vbi12-20-39.png
+mv Screenshot*12-28-26.png vbi12-28-26.png
+mv Screenshot*12-28-38.png vbi12-28-38.png
+mv Screenshot*12-28-55.png vbi12-28-55.png
+mv Screenshot*12-29-11.png vbi12-29-11.png
+mv Screenshot*12-29-33.png vbi12-29-33.png
+mv Screenshot*12-29-42.png vbi12-29-42.png
+mv Screenshot*12-33-19.png vbi12-33-19.png
+mv Screenshot*12-33-28.png vbi12-33-28.png
+mv Screenshot*12-33-38.png vbi12-33-38.png
+mv Screenshot*12-33-52.png vbi12-33-52.png
+mv Screenshot*12-34-03.png vbi12-34-03.png
+mv Screenshot*12-34-14.png vbi12-34-14.png
+mv Screenshot*12-35-43.png vbi12-35-43.png
+mv Screenshot*12-35-58.png vbi12-35-58.png
+mv Screenshot*12-37-01.png vbi12-37-01.png
+mv Screenshot*12-37-30.png vbi12-37-30.png
+mv Screenshot*12-39-35.png vbi12-39-35.png
+mv Screenshot*12-40-02.png vbi12-40-02.png
+mv Screenshot*12-40-42.png vbi12-40-42.png
+			  
