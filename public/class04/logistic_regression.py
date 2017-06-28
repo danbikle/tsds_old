bf0044864c782445a4226a3b6955352efab01fca
Dan Bikle <bikle@bikle.com>
2017-06-27 18:06:34 -0700

    more

--- public/class04/logistic_regression.py
+++ public/class04/logistic_regression.py
@@ -1,18 +1,21 @@
-# logistic_regression.py
+"""
+logistic_regression.py
 
-# This script is a simple demo of using scikit-learn to run Logistic Regression.
+This script is a simple demo of using scikit-learn to run Logistic Regression.
 
-# Demo:
-# python logistic_regression.py
+Demo:
+python logistic_regression.py
+"""
 
 import pandas as pd
 import numpy  as np
 import pdb
+from sklearn import linear_model
 
 # I should use Pandas here:
 data_df  = pd.read_csv('http://www.spy611.com/csv/allpredictions.csv')
 train_sr = (data_df['cdate'] > '1990') & (data_df['cdate'] < '2016')
-test_sr  =  data_df['cdate'] == '2016-06-23'
+test_sr  =  data_df['cdate'] == '2017-06-26'
 train_df = data_df[['pctlead', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][train_sr]
 test_df  = data_df[['pctlead', 'pctlag1', 'pctlag2', 'pctlag4', 'pctlag8', 'pctlag16']][test_sr]
 
@@ -23,7 +26,6 @@ y_train_a = train_a[:,0]
 train_median  = np.median(y_train_a)
 label_train_a = y_train_a > train_median
 # I should learn from x_train_a,label_train_a:
-from sklearn import linear_model
 clf_lr = linear_model.LogisticRegression()
 clf_lr.fit(x_train_a, label_train_a)
 
@@ -35,7 +37,7 @@ xr_a     = xf_a.reshape(1, -1)
 
 aprediction_lr = clf_lr.predict_proba(xr_a)[0,1]
 
-print('The probability that 2016-06-24 will go up is...')
+print('The probability that 2017-06-26 will go up is...')
 print(aprediction_lr)
 
 'bye'
